3e37c578575abe81df706d46c8ad80bbf14874fc
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-01-18 19:27:07 +0900

    Time: 80 ms (45.39%), Space: 42.3 MB (35.56%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -5,22 +5,17 @@
 var findPeakElement = function(nums) {
     var left = 0;
     var right = nums.length - 1;
-    var pivot = parseInt((left + right)/2);
-    var pivot_next = pivot + 1;
     
     if (nums.length == 1) {
         return 0;
     }
     
     while ( left != right ) {
-        if ( nums[pivot] < nums[pivot_next] ) {
-            left = pivot_next;
-            pivot = parseInt((left + right)/2);
-            pivot_next = pivot + 1;
+        var pivot = parseInt((left + right)/2);
+        if ( nums[pivot] < nums[pivot + 1] ) {
+            left = pivot + 1;
         } else {
             right = pivot;
-            pivot = parseInt((left + right)/2);
-            pivot_next = pivot + 1;
         }
     }
     return left;
