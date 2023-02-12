60021061e7cc8d5ed4c34198b05b9643acac5248
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-02-12 19:40:14 +0900

    Time: 84 ms (74.82%), Space: 48.4 MB (48.95%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -6,10 +6,8 @@ var validPalindrome = function(s) {
     let start = 0;
     let end = s.length - 1;
     
-    console.log(s, start, end);
     while ( start < end ) {
         if ( s[start] != s[end] ) {
-            console.log( isPalindrome(s, start+1, end) || isPalindrome(s, start, end-1) )
             return ( isPalindrome(s, start+1, end) || isPalindrome(s, start, end-1) )
         }
         start++;
@@ -20,7 +18,6 @@ var validPalindrome = function(s) {
 };
 
 function isPalindrome(str, start, end) {
-    console.log('called isPalindrome')
     while ( start < end ) {
         if ( str[start] != str[end] ) {
             return false
