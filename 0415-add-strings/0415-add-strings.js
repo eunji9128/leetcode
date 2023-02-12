62bf29e60ef2fe0fe0431ca65b68b0efb0900964
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-02-12 21:00:41 +0900

    Time: 74 ms (59.18%), Space: 44.5 MB (31.84%) - LeetHub

--- 0415-add-strings/0415-add-strings.js
+++ 0415-add-strings/0415-add-strings.js
@@ -10,14 +10,11 @@ var addStrings = function(num1, num2) {
     let sum = '';
     
     for ( var i = 0; i < Math.max(len1, len2); i++ ) {
-        var int1 = parseInt(num1[len1 - 1 - i]);
-        var int2 = parseInt(num2[len2 - 1 - i]);
-        
-        isNaN(int1) ? int1 = 0 : null;
-        isNaN(int2) ? int2 = 0 : null;
+        var int1 = parseInt(num1[len1 - 1 - i] || 0);
+        var int2 = parseInt(num2[len2 - 1 - i] || 0);
         
         sum = String((carry + int1 + int2) % 10) + sum;
-        carry = parseInt(( carry + int1 + int2 ) / 10);
+        carry = (carry + int1 + int2) > 9 ? 1 : 0;
     }
     
     if ( carry != 0) {
