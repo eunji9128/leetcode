f7a36b759e85564f3df0a27d688126657ed84bd7
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-02-16 21:42:16 +0900

    Time: 82 ms (84.76%), Space: 51.5 MB (48.69%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    let stack = [];
+    
+    for (let char of s){
+        if (stack[stack.length - 1] === char && stack.length !== 0){
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.join("")
+};
