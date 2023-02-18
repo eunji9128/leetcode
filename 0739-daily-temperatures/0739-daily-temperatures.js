622b2a2d53605609191529874e92838ee354dd5b
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-02-18 16:13:41 +0900

    Time: 285 ms (74.72%), Space: 62.5 MB (97.06%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,50 +3,6 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-//     let stack = [];
-//     let num = -Infinity;
-//     let t = temperatures;
-//     let answer = new Array(t.length).fill(0);
-    
-//     for (let i = t.length - 1; i >= 0; i--){
-//         num = t[i];
-        
-//         if(stack.length === 0){
-//             stack.push({
-//                 value: num,
-//                 idx: i
-//             });
-//             continue;
-//         }
-        
-//         let top = stack[stack.length - 1].value;
-//         let topIdx = stack[stack.length - 1].idx;
-//         if (num < top){
-//             answer[i] = topIdx - i;
-//             stack.push({
-//                 value: num,
-//                 idx: i
-//             });
-//         } else if (num >= top){
-//             while(num >= top){
-//                 stack.pop();
-//                 if (stack.length === 0){
-//                    topIdx = i;
-//                    break;
-//                 }
-//                 top = stack[stack.length - 1].value;
-//                 topIdx = stack[stack.length - 1].idx;
-//             }
-//             answer[i] = topIdx - i;
-//             stack.push({
-//                 value: num,
-//                 idx: i
-//             });
-//         }
-//     }
-
-//     return answer;
-    
     let stack = [];
     let answer = new Array(temperatures.length).fill(0);
     let len = temperatures.length;
