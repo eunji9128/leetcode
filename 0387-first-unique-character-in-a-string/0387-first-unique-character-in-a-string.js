e8a4561edde54703f2b875f6e249be8fa89732bd
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-02-18 22:05:39 +0900

    Time: 120 ms (48.11%), Space: 50.7 MB (9.92%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const map = new Map();
+    s = s.split("");
+    
+    for(let i = 0; i < s.length; i++){
+        if (map.get(s[i]) === undefined) {
+            map.set(s[i], i);
+        } else {
+            map.set(s[i], -1);
+        }
+    }
+    
+    for (let char of s){
+        if (map.get(char) >= 0) {
+            return map.get(char)
+        }
+    }
+    return -1;
+};
