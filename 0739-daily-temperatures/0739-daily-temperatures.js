d3d79e58a9c2ae30637af3c4d1231f9cd3a0aed9
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-02-18 16:13:23 +0900

    Time: 243 ms (99.60%), Space: 62.7 MB (96.06%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,47 +3,61 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    let stack = [];
-    let num = -Infinity;
-    let t = temperatures;
-    let answer = new Array(t.length).fill(0);
+//     let stack = [];
+//     let num = -Infinity;
+//     let t = temperatures;
+//     let answer = new Array(t.length).fill(0);
     
-    for (let i = t.length - 1; i >= 0; i--){
-        num = t[i];
+//     for (let i = t.length - 1; i >= 0; i--){
+//         num = t[i];
         
-        if(stack.length === 0){
-            stack.push({
-                value: num,
-                idx: i
-            });
-            continue;
-        }
+//         if(stack.length === 0){
+//             stack.push({
+//                 value: num,
+//                 idx: i
+//             });
+//             continue;
+//         }
         
-        let top = stack[stack.length - 1].value;
-        let topIdx = stack[stack.length - 1].idx;
-        if (num < top){
-            answer[i] = topIdx - i;
-            stack.push({
-                value: num,
-                idx: i
-            });
-        } else if (num >= top){
-            while(num >= top){
-                stack.pop();
-                if (stack.length === 0){
-                   topIdx = i;
-                   break;
-                }
-                top = stack[stack.length - 1].value;
-                topIdx = stack[stack.length - 1].idx;
-            }
-            answer[i] = topIdx - i;
-            stack.push({
-                value: num,
-                idx: i
-            });
-        }
-    }
+//         let top = stack[stack.length - 1].value;
+//         let topIdx = stack[stack.length - 1].idx;
+//         if (num < top){
+//             answer[i] = topIdx - i;
+//             stack.push({
+//                 value: num,
+//                 idx: i
+//             });
+//         } else if (num >= top){
+//             while(num >= top){
+//                 stack.pop();
+//                 if (stack.length === 0){
+//                    topIdx = i;
+//                    break;
+//                 }
+//                 top = stack[stack.length - 1].value;
+//                 topIdx = stack[stack.length - 1].idx;
+//             }
+//             answer[i] = topIdx - i;
+//             stack.push({
+//                 value: num,
+//                 idx: i
+//             });
+//         }
+//     }
 
+//     return answer;
+    
+    let stack = [];
+    let answer = new Array(temperatures.length).fill(0);
+    let len = temperatures.length;
+    
+    for (let i = len - 1; i >= 0; i--){
+        while(stack.length && temperatures[i] >= temperatures[stack[stack.length - 1]]){
+            stack.pop();
+        }
+        answer[i] = stack[stack.length - 1] - i || 0;
+        stack.push(i);
+    }
+    
     return answer;
 };
