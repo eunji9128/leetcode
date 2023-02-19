f19eb87c4d9bd09a5f6aff9cd68a068428d360f5
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-02-19 17:06:48 +0900

    Time: 59 ms (82.73%), Space: 42 MB (33.63%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -4,23 +4,22 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-    let patMap = new Map();
-    let strMap = new Map();
     s = s.split(' ');
     
-    if (pattern.length !== s.length) return false;
-    
-    for (let i = 0; i < pattern.length; i++){
-        if (!patMap.has(pattern[i]) && !strMap.has(s[i])) {
-            patMap.set(pattern[i], s[i]);
-            strMap.set(s[i], pattern[i]);
-        } else if (patMap.get(pattern[i]) !== s[i]) {
-            return false;
-        }
+    const patternCheck = function(arr){
+        let map = new Map();
+        let id = 0;
+        let res = '';
         
-        if (pattern[i] !== strMap.get(patMap.get(pattern[i]))) {
-            return false;
+        for (let item of arr){
+            if (!map.has(item)) {
+                map.set(item, ++id);
+            }
+            res = res + map.get(item);
         }
-    }
-    return true;
+        return res;
+    };
+    
+    return patternCheck(pattern) === patternCheck(s);
+    
 };
