5b349ba0743b3ed37d4567b07bd99b19146abd81
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-02-24 17:34:20 +0900

    Time: 74 ms (60.80%), Space: 43.3 MB (98.87%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -10,29 +10,22 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
+var mergeTwoLists = function(list1, list2) {    
     let dummy = new ListNode(0, null);
     let crntNode = dummy;
-    let node1 = list1;
-    let node2 = list2;
     
-    while (node1 || node2) {
-        if (!node1) {
-            crntNode.next = node2;
-            node2 = node2.next;
-        } else if (!node2) {
-            crntNode.next = node1;
-            node1 = node1.next;
-        } else if (node1.val <= node2.val) {
-            crntNode.next = node1;
-            node1 = node1.next;
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            crntNode.next = list1;
+            list1 = list1.next;
         } else {
-            crntNode.next = node2;
-            node2 = node2.next;
+            crntNode.next = list2;
+            list2 = list2.next;
         }
         crntNode = crntNode.next;
     };
     
+    crntNode.next = (list1 || list2);
     return dummy.next;
     
 };
