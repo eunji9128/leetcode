0627bfb55382efa5bd74343311f91f3d22db8d35
Selen (Eun-Ji Yang) <80051721+eunji9128@users.noreply.github.com>
2023-04-26 21:39:27 +0900

    Time: 61 ms (56.37%), Space: 42.3 MB (67.29%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,11 +3,8 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let sum = nums[0];
-    
     for (let i=1; i<nums.length; i++) {
-        nums[i] = sum + nums[i];
-        sum = nums[i];
+        nums[i] += nums[i-1];
     }
     
     return nums
