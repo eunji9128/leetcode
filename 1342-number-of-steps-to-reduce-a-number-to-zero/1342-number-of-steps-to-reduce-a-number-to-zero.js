dff161029df54d1117065132a59b392c4e61d14d
Selen (Eun-Ji Yang) <80051721+eunji9128@users.noreply.github.com>
2023-04-27 21:26:14 +0900

    Time: 61 ms (46.16%), Space: 42.2 MB (31.60%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -6,13 +6,9 @@ var numberOfSteps = function(num) {
     let count = 0;
     
     while (num > 0) {
-        if (num % 2 === 0) {
-            num /= 2;
-            count += 1;
-        } else {
-            num -= 1;
-            count += 1;
-        }
+        if (num % 2 === 0) num /= 2;
+        else num -= 1;
+        count++;
     }
     
     return count;
